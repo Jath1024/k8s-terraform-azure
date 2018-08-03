9430a38fee837ef8eb7dfb593fbfc1e5d9e9dbf7
James Thompson <jtbb58@googlemail.com>
2018-08-03 18:10:03 +0100

    updates

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -11,19 +11,19 @@ resource "random_integer" "random_int" {
 resource azurerm_network_security_group "aks_advanced_network" {
   name                = "akc-${random_integer.random_int.result}-nsg"
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.akc-rg.name}"
+  resource_group_name = "${azurerm_resource_group.k8s.name}"
 }
 
 resource "azurerm_virtual_network" "aks_advanced_network" {
   name                = "akc-${random_integer.random_int.result}-vnet"
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.akc-rg.name}"
+  resource_group_name = "${azurerm_resource_group.k8s.name}"
   address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "aks_subnet" {
   name                      = "akc-${random_integer.random_int.result}-subnet"
-  resource_group_name       = "${azurerm_resource_group.akc-rg.name}"
+  resource_group_name       = "${azurerm_resource_group.k8s.name}"
   network_security_group_id = "${azurerm_network_security_group.aks_advanced_network.id}"
   address_prefix            = "10.1.0.0/24"
   virtual_network_name      = "${azurerm_virtual_network.aks_advanced_network.name}"
